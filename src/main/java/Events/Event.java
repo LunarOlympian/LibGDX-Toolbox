3d8aecf8e6e75bf17549d56e9724da6067a2d671
LunarOlympian <sebastiandrayne@gmail.com>
2024-02-26 01:46:25 +0000

    Updated method names to make more sense. Implemented a pause function to enable the pausing of events.

--- src/main/java/Events/Event.java
+++ src/main/java/Events/Event.java
@@ -3,16 +3,31 @@ package Events;
 public class Event {
     // Intended to make repetitive events (Such as rotating something) able to be done easily.
     boolean active = true; // Upon declaration the event is active;
+    boolean paused = false;
     public void trigger() {
         // Just so the event automatically ends.
-        endEvent();
+        if(!paused) {
+            actions();
+        }
     }
 
+    // Triggers an event unless paused.
     public void trigger(Object... arguments) {
-        endEvent();
+        if(!paused) {
+            actions(arguments);
+        }
     }
 
-    public void endEvent() {
+    public void actions(Object... arguments) {
+        end();
+    }
+
+    // Temporary pause.
+    public void pause(boolean pauseValue) {
+        paused = pauseValue;
+    }
+
+    public void end() {
         active = false;
     }
 
