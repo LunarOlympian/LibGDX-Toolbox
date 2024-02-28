daf8277d34e40dd37dc14a4686fb68d7164c088b
LunarOlympian <sebastiandrayne@gmail.com>
2024-02-28 11:52:18 +0000

    Made some methods in Events final, and made some changes to event pausing (namely added a way to check if an event is paused and toggle its pause state)

--- src/main/java/Events/Event.java
+++ src/main/java/Events/Event.java
@@ -4,34 +4,47 @@ public class Event {
     // Intended to make repetitive events (Such as rotating something) able to be done easily.
     boolean active = true; // Upon declaration the event is active;
     boolean paused = false;
-    public void trigger() {
+    public final void trigger() {
         // Just so the event automatically ends.
         if(!paused) {
             actions();
         }
     }
 
-    // Triggers an event unless paused.
-    public void trigger(Object... arguments) {
+    // Triggers an event unless paused. Final as allowing overriding may cause confusion.
+    public final void trigger(Object... arguments) {
         if(!paused) {
             actions(arguments);
         }
     }
 
+    // Goal is for this to be the only method that you should be allowed to override.
     public void actions(Object... arguments) {
         end();
     }
 
-    // Temporary pause.
-    public void pause(boolean pauseValue) {
+    // Handle pausing. Both of these are final as I think allowing overriding could cause issues.
+    // ----------
+    public final void pause(boolean pauseValue) {
         paused = pauseValue;
     }
 
-    public void end() {
+    public final boolean getPaused() {
+        return paused;
+    }
+
+    public final void togglePause() {
+        paused = !paused;
+    }
+    // ----------
+
+
+
+    public final void end() {
         active = false;
     }
 
-    public boolean getActive() {
+    public final boolean getActive() {
         return active;
     }
 
